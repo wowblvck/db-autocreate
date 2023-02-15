a90c75b0c5d3b9ad3d4ebca2efa1f5eba602ba84
Indar Basto <indar.basto@gmail.com>
2023-02-15 12:39:50 +0300

    feat: removed unnecessary letters

--- utils/class-name-generator.js
+++ utils/class-name-generator.js
@@ -1,7 +1,7 @@
 const generateClassName = () => {
   let result = "";
   let nums = [1, 2, 3, 4, 5];
-  let letters = ["А", "Б", "В", "Г", "Д", "Е", "Ж", "И", "К", "Л", "М", "Н", "П", "Р", "С", "Т", "У", "Ф", "Х", "Ц", "Ч", "Ш", "Ы", "Э", "Ю", "Я"];
+  let letters = ["А", "Б", "В"];
   
   let randomNum = nums.splice(Math.floor(Math.random() * nums.length), 1)[0];
   let randomLetter = letters.splice(Math.floor(Math.random() * letters.length), 1)[0];
