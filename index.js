e77bd002178d92ce07e9461335688a6b948ada35
Indar Basto <indar.basto@gmail.com>
2023-02-24 14:59:49 +0300

    fix: school adress

--- index.js
+++ index.js
@@ -9,36 +9,35 @@ import createSubjects from "./components/add-subjects.js";
 import createInfo from "./components/add-info.js";
 
 const run = async () => {
-  // await Promise.all([createRole()]).then(() => {
-  //   console.log("All roles will be created!");
-  //   Promise.all([createUser()]).then(() => {
-  //     console.log("All users will be created!");
-  //     Promise.all([createTeacher()]).then(() => {
-  //       console.log("All teachers will be created!");
-  //       Promise.all([createClass()]).then(() => {
-  //         console.log("All classes will be created!");
-  //         Promise.all([createChildren()]).then(() => {
-  //           console.log("All childrens will be created!");
-  //         })
-  //         Promise.all([createQuarter()]).then(() => {
-  //           console.log("All quarters will be created!");
-  //           Promise.all([createSubjects()]).then(() => {
-  //             console.log("All subjects will be created!");
-  //           })
-  //         })
-  //       })
-  //     })
-  //   })
-  // })
-  // await Promise.all([createNews()]).then(() => {
-  //   console.log("All news will be created!");
-  // })
+  await Promise.all([createRole()]).then(() => {
+    console.log("All roles will be created!");
+    Promise.all([createUser()]).then(() => {
+      console.log("All users will be created!");
+      Promise.all([createTeacher()]).then(() => {
+        console.log("All teachers will be created!");
+        Promise.all([createClass()]).then(() => {
+          console.log("All classes will be created!");
+          Promise.all([createChildren()]).then(() => {
+            console.log("All childrens will be created!");
+          })
+          Promise.all([createQuarter()]).then(() => {
+            console.log("All quarters will be created!");
+            Promise.all([createSubjects()]).then(() => {
+              console.log("All subjects will be created!");
+            })
+          })
+        })
+      })
+    })
+  })
+  await Promise.all([createNews()]).then(() => {
+    console.log("All news will be created!");
+  })
   
   const info = await createInfo();
   if (info) {
     console.log("Info is created!");
   }
-
 }
 
 run();
